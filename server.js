82a670d34bf7477fa67249ab10a6bb1459602e79
aneeshkumarms <aneeshkumarms@gmail.com>
2017-02-16 10:31:25 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
-  res.send(createTemplate(articles[article-one]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two',function(req,res){
